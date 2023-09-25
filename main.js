730b49d27bca0c544bc66dd0ee2c5b8e74caba65
Loukas Vovlas <vovlasloukas@gmail.com>
2023-09-25 02:10:02 +0200

    body overflow hidden when menu opens

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const navToggle = document.querySelector(".mobile-nav-toggle");
 const navigation = document.querySelector(".navigation");
 const iconMenu = document.querySelector(".icon-menu");
 const iconMenuClose = document.querySelector(".icon-menu-close");
+const body = document.querySelector("body");
+const navToggleStyle = window.getComputedStyle(navToggle);
 
 navToggle.addEventListener('click', () => {
     navigation.hasAttribute("data-visible")
@@ -12,5 +14,26 @@ navToggle.addEventListener('click', () => {
     temp = iconMenu.getAttribute("style");
     iconMenu.setAttribute("style", iconMenuClose.getAttribute("style"));
     iconMenuClose.setAttribute("style", temp);
+
+    if (iconMenuClose.getAttribute("style") == "display: block;") {
+        body.setAttribute("style", "overflow-y: hidden");
+    } else {
+        body.setAttribute("style", "overflow-y: visible");
+    }
+
     navigation.toggleAttribute("data-visible");
 });
+
+
+window.displayListener = function () {
+    if (navToggleStyle.getPropertyValue("display") == "none") {
+        body.setAttribute("style", "overflow-y: visible;");
+    } else {
+        if (navigation.hasAttribute("data-visible")) {
+            window.scrollTo(0, 0);
+            body.setAttribute("style", "overflow-y: hidden");
+        } else {
+            body.setAttribute("style", "overflow-y: visible");
+        }
+    }
+}
